45dce04fcaa22a775555f9eb21329c93f24d3a7a
arkadiy <arkadiiozerov13@gmail.com>
2020-02-16 13:30:26 +0300

    clear project

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,11 +1,4 @@
 import { Component } from '@angular/core';
-import {AuthService} from './auth.service';
-
-export interface Post {
-  title: string;
-  text: string;
-  id?: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -13,19 +6,6 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private auth: AuthService) {
-  }
-
-  posts: Post[] = [
-    { title: 'Post 1', text: 'text...', id: '1'},
-    { title: 'Post 2', text: 'text...', id: '2'}
-  ];
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post);
-  }
-
-  removePost(id: string) {
-    this.posts = this.posts.filter((post: Post) => post.id !== id);
+  constructor() {
   }
 }
